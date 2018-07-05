67cd933a9e3299dc7e1f7d7a3243df892ad0ad97
Kenzo Nojima <nojiken@nojimac.local>
2018-07-05 11:48:24 +0900

    add MemoDetailScreen

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,16 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
+    const { style, color } = this.props;
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#E31676';
+    }
     return (
-      <View style={styles.circleButton}>
-        <Text style={styles.circleBottenTitle}>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleBottenTitle, { color: textColor }]}>
           { this.props.children }
         </Text>
       </View>
@@ -17,7 +24,6 @@ const styles = StyleSheet.create({
   circleBottenTitle: {
     fontSize: 32,
     lineHeight: 32,
-    color: '#fff',
   },
   circleButton: {
     position: 'absolute',
@@ -25,7 +31,6 @@ const styles = StyleSheet.create({
     right: 32,
     width: 48,
     height: 48,
-    backgroundColor: '#E31676',
     borderRadius: 24,
     justifyContent: 'center',
     alignItems: 'center',
